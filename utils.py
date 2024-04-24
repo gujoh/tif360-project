075185e27b1d884f6329f5200f0a5c9a40630deb
gujoh <gustafjohanson@live.com>
2024-04-24 12:08:46 +0200

    utils file to make scaling up easier

---
+++ utils.py
@@ -0,0 +1,36 @@
+import os 
+from PIL import Image 
+from torch.utils.data import Dataset as TorchDataset
+from matplotlib import pyplot as plt
+from torchvision.transforms import functional as v_F
+import torch
+
+# Dataset to store the images. 
+class Dataset(TorchDataset):
+    def __init__(self, main_dir, transform):
+        self.main_dir = main_dir
+        self.transform = transform
+        self.total_imgs = os.listdir(main_dir)
+
+    def __len__(self):
+        return len(self.total_imgs)
+    
+    def __getitem__(self, idx):
+        img_loc = os.path.join(self.main_dir, self.total_imgs[idx])
+        image = Image.open(img_loc).convert("RGB")
+        tensor_image = self.transform(image)
+        return tensor_image
+
+def get_data(transform=None, batch_size=32, shuffle=True):
+    paintings = Dataset("./data/monet_jpg", transform)
+    photos = Dataset("./data/photo_jpg", transform)
+    painting_loader = torch.utils.data.DataLoader(paintings, batch_size=batch_size, shuffle=shuffle)
+    photo_loader = torch.utils.data.DataLoader(paintings, batch_size=batch_size, shuffle=shuffle)
+    return paintings, photos, painting_loader, photo_loader
+    
+# Shows an image. 
+def show_img(img):
+    plt.imshow(v_F.to_pil_image(img))
+    plt.xticks([])
+    plt.yticks([])
+    plt.show()
